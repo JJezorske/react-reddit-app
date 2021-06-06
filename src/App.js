1b6c33af482a5331645ed04c71745574f7298878
JJ <79755422+JJezorske@users.noreply.github.com>
2021-06-06 20:01:04 +0100

    first draft with basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Article from './components/Article';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('Rabbits');
+
+  useEffect( () => {
+    fetch("https://www.reddit.com/r/"+ subreddit +".json").then(Response => {
+      if (Response.status !== 200) {
+        console.log('Fetch Error');
+        return;
+      }
+      Response.json().then(Response => {
+        if (Response != null) {
+          setArticles(Response.data.children);
+        }
+      })
+    })
+  }, [subreddit]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" className="input" value={subreddit} onChange={e => setSubreddit(e.target.value)}></input>
       </header>
+      <div className="articles">
+        {
+          (articles !== null) ? articles.map((article, index) => <Article key={index} article={article.data} />) : '' 
+        }
+      </div>
     </div>
   );
 }
